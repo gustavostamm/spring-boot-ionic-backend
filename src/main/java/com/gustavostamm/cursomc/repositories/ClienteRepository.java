a292e1ad93615a73f8f36eda5a5dc6844afe7121
gustavostamm <gustavo.stamm@gmail.com>
2018-08-16 21:07:07 -0300

    Ṽalidação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/gustavostamm/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/gustavostamm/cursomc/repositories/ClienteRepository.java
@@ -3,10 +3,11 @@ package com.gustavostamm.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gustavostamm.cursomc.domain.Categoria;
 import com.gustavostamm.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@org.springframework.transaction.annotation.Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
